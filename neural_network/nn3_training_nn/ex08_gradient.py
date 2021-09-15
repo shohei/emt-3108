d75bf8b81d7985cacca2b56f346fde36ef59a8eb
Shohei Aoki <shoaok@gmail.com>
2021-09-15 15:34:01 +0300

    update

--- neural_network/nn3_training_nn/ex08_gradient.py
+++ neural_network/nn3_training_nn/ex08_gradient.py
@@ -1,7 +1,7 @@
 import numpy as np
 from ex07_partial_diff import function_2
 
-def numerical_gradient(f, x):
+def numerical_gradient_1d(f, x):
     h = 1e-4 # 0.0001
     grad = np.zeros_like(x)
     for idx in range(x.size):
@@ -14,8 +14,39 @@ def numerical_gradient(f, x):
         x[idx] = tmp_val 
     return grad
 
+
+def numerical_gradient_2d(f, X):
+    if X.ndim == 1:
+        return numerical_gradient_1d(f, X)
+    else:
+        grad = np.zeros_like(X)
+        
+        for idx, x in enumerate(X):
+            grad[idx] = numerical_gradient_1d(f, x)
+        return grad
+
+def numerical_gradient(f, x):
+    h = 1e-4 # 0.0001
+    grad = np.zeros_like(x)
+    
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+    while not it.finished:
+        idx = it.multi_index
+        tmp_val = x[idx]
+        x[idx] = tmp_val + h
+        fxh1 = f(x) # f(x+h)
+        
+        x[idx] = tmp_val - h 
+        fxh2 = f(x) # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2*h)
+        
+        x[idx] = tmp_val # Revert value
+        it.iternext()   
+        
+    return grad
+
 if __name__=="__main__":
-    print(numerical_gradient(function_2, np.array([3.0, 4.0]))) # array([ 6., 8.])
-    print(numerical_gradient(function_2, np.array([0.0, 2.0]))) # array([ 0., 4.]) 
-    print(numerical_gradient(function_2, np.array([3.0, 0.0]))) # array([ 6., 0.])
+    print(numerical_gradient_1d(function_2, np.array([3.0, 4.0]))) # array([ 6., 8.])
+    print(numerical_gradient_1d(function_2, np.array([0.0, 2.0]))) # array([ 0., 4.]) 
+    print(numerical_gradient_1d(function_2, np.array([3.0, 0.0]))) # array([ 6., 0.])
  
--- neural_network/nn3_training_nn/ex13_gradient_cost_function.py
+++ neural_network/nn3_training_nn/ex13_gradient_cost_function.py
@@ -4,6 +4,7 @@ import numpy as np
 from nn2_neural_network.ex12_softmax import softmax
 from ex04_cross_entropy_error_minibatch import cross_entropy_error
 from ex08_gradient import numerical_gradient
+import pdb
              
 class simpleNet:
     def __init__(self):
@@ -18,11 +19,14 @@ class simpleNet:
 
 if __name__=="__main__":
     net = simpleNet()
-    print(net.W) #[ 0.85557411,  0.03563661,  0.69422093]
     x = np.array([0.6, 0.9])
     p = net.predict(x)
-    print(p) #[ 1.13282549 0.66052348 1.20919114] 
-    print(np.argmax(p))  #2
     t = np.array([0, 0, 1]) 
-    print(net.loss(x, t)) #0.92806853663411326
+
+    f = lambda w: net.loss(x,t)
+    dW = numerical_gradient(f, net.W)
+    print(dW)
+
+
+
  
