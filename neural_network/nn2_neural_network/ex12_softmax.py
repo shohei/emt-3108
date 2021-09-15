cb995d0626938736ed27b46386dd72677f851bd2
Shohei Aoki <shoaok@gmail.com>
2021-09-15 13:08:07 +0300

    add .gitignore

---
+++ neural_network/nn2_neural_network/ex12_softmax.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def softmax(a):
+    exp_a = np.exp(a)
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+if __name__=="__main__":
+    a = np.array([0.3, 2.9, 4.0])
+    y = softmax(a)
+    print(y) #[ 0.01821127 0.24519181 0.73659691] 
+    print(np.sum(y)) # 1.0
