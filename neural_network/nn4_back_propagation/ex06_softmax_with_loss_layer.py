fb34f8858bba28a81ee3754acd77080693b1e16a
Shohei Aoki <shoaok@gmail.com>
2021-09-16 14:29:36 +0300

    add softmax_with_loss_layer

---
+++ neural_network/nn4_back_propagation/ex06_softmax_with_loss_layer.py
@@ -0,0 +1,19 @@
+import os, sys
+sys.path.append(os.path.abspath("../"))
+from nn3_training_nn.ex04_cross_entropy_error_minibatch import cross_entropy_error
+from nn2_neural_network.ex13_softmax_improved import softmax
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None # Loss
+        self.y = None # softmax output
+        self.t = None # Label (one-hot vector)
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        return dx
