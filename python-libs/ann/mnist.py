f9492d04b2775a61a515fec4e82747f7558851e1
Shohei Aoki <shoaok@gmail.com>
2021-09-20 14:02:53 +0300

    add scikit-learn neural network

---
+++ python-libs/ann/mnist.py
@@ -0,0 +1,31 @@
+import matplotlib.pyplot as plt
+from sklearn import datasets
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import train_test_split
+
+digits = datasets.load_digits()
+
+_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+for ax, image, label in zip(axes, digits.images, digits.target):
+    ax.set_axis_off()
+    ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    ax.set_title('label: %i' % label)
+
+n_samples = len(digits.images)
+data = digits.images.reshape((n_samples, -1))
+
+X_train, X_test, y_train, y_test = train_test_split(
+    data, digits.target, test_size=0.5, shuffle=False)
+clf = MLPClassifier(random_state=1, max_iter=300)
+clf.fit(X_train, y_train)
+predicted = clf.predict(X_test)
+print("accuracy",clf.score(X_test, y_test))
+
+_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
+for ax, image, prediction in zip(axes, X_test, predicted):
+    ax.set_axis_off()
+    image = image.reshape(8, 8)
+    ax.imshow(image, cmap=plt.cm.gray_r, interpolation='nearest')
+    ax.set_title(f'Prediction: {prediction}')
+
+plt.show()
