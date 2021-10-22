4d6c429fd53daafc2cdcd4d4541c6c1e3fab1009
Shohei Aoki <shoaok@gmail.com>
2021-10-22 15:10:36 +0900

    update

--- misc/definite_integral.py
+++ misc/definite_integral.py
@@ -1,9 +1,11 @@
 from sympy import *
 x=Symbol('x')
 
+# This calculates the definite integral of y=x^2 from -1 to 1
 y = integrate(x**2, (x, -1, 1))  
 print(y)
 print(y.evalf())
 
+# This calculates the definite integral of y=sin(x) from 0 to pi/2
 y = integrate(sin(x), (x, 0, pi/2)) 
 print(y)
