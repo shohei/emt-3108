78f292292b56b3c0b7297a3eecb9d8dec726d216
Shohei Aoki <shoaok@gmail.com>
2021-11-20 11:34:57 +0900

    update rl5

--- practical/reinforcement_learning/rl5.py
+++ practical/reinforcement_learning/rl5.py
@@ -10,6 +10,11 @@ from rl1 import advance_state
 from rl2 import max_Qval
 from rl4 import epsilon_greedy
 
+def update_Qtable(alpha, gamma, reward, Qtable, Q_max):
+    # Describe your code here
+
+    return Q
+
 if __name__=="__main__":
     Qtable = np.zeros((num_s,num_a))
     s = 0
@@ -19,7 +24,7 @@ if __name__=="__main__":
             a = epsilon_greedy(epsilon, s, num_a, Qtable)
             reward, s2 = advance_state(s,a)
             Q_max = max_Qval(s2, Qtable)
-            Qtable[s][a] = (1-alpha)*Qtable[s][a] + alpha*(reward+gamma*Q_max)
+            Qtable[s][a] = update_Qtable(alpha, gamma, Qtable, Q_max) 
             s = s2
             if reward>0:
                 break
